61a27d07e3d632b66b7fc740813c209924311366
Tyra <tyra.ekstrand@elev.ga.ntig.se>
2021-05-27 15:52:00 +0200

    add remove button, add overline when click

--- script.js
+++ script.js
@@ -5,24 +5,43 @@ let todoOutput = document.querySelector("#todo-output");
 let todoList = [];
 
 todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    todoList.unshift(todoInput.value);
-    update();
-})
+  e.preventDefault();
+  todoList.unshift(todoInput.value);
+  update(todoInput.value);
+  console.log();
+  todoInput.value = "";
+});
 
-function update() {
-    todoOutput.innerHTML = "";
-    todoList.forEach((e) => {
-        let listItem = document.createElement("li");
-        listItem.innerText = e;
-        listItem.addEventListener("click",  removeItem);
-        todoOutput.append(listItem);
-    })
+function update(text) {
+  let listDiv = document.createElement("div");
+  listDiv.classList.add("todo-item");
+  let listItem = document.createElement("li");
+  listItem.innerText = text;
+  listItem.addEventListener("click", crossItem);
+  listDiv.append(listItem);
+  let listButton = document.createElement("button");
+  listButton.addEventListener("click", removeItem);
+  listButton.innerHTML = '<i class="fas fa-trash"></i>';
+  listDiv.append(listButton);
+  todoOutput.append(listDiv);
+}
+
+function crossItem(event) {
+  event.target.classList.add("removed");
+
+  console.log(event.target.classList);
+  // let itemToRemove = event.target.innerText;
+  // todoList = todoList.filter((item) => item !== itemToRemove);
 }
 
 function removeItem(event) {
-    let itemToRemove = event.target.innerText;
-    todoList = todoList.filter((item) => item !== itemToRemove);
-    update();
-}
+  let item = event.target;
+  let parent = item.parentElement;
 
+  while (parent.nodeName !== "DIV") {
+    let newParent = parent.parentElement;
+    parent = newParent;
+  }
+
+  parent.classList.add("noshow");
+}
